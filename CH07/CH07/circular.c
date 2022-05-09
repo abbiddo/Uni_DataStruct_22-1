f8504ecc3e4a9a24ab6b34996d1b2f219c07eb4a
abbiddo@naver.com <abbiddo@naver.com>
2022-05-10 00:53:26 +0900

    10주차 - 2 : circular list

---
+++ CH07/CH07/circular.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct ListNode {
+	element data;
+	struct ListNode* link;
+}ListNode;
+
+ListNode* insert_first(ListNode* head, element data) {
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		node->link = head;
+		head = node;
+
+		/*head = node;
+		head->link = head;*/
+
+		/*head = node;
+		node->link = head;*/
+		// 대입하고 node를 변경하면 head에도 변경값이 반영되나?
+	}
+	else {
+		node->link = head->link;
+		head->link = node;
+	}
+	return head;
+}
+
+ListNode* insert_last(ListNode* head, element data) {
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (!head) {
+		head = node;
+		node->link = head;
+
+		/*node->link = head->link;
+		head = node;*/
+
+		// 얘도 순서 무관? 
+	}
+	else {
+		node->link = head->link;
+		head->link = node;             // 왜?
+		head = node;
+	}
+	return head;
+}
+void print_list(ListNode* head) {
+	ListNode* p;
+	if (!head) return;
+	p = head->link;
+	do {
+		printf("%d->", p->data);
+		p = p->link;
+	} while (p != head->link);
+}
+
+// 실습 : 리스트 개수 출력
+int count_list(ListNode* head) {
+	int cnt = 0;
+	ListNode* p;
+
+	if (!head) return 0;
+
+	p = head->link;
+	do {
+		p = p->link;
+		cnt++;
+	} while (p != head->link);
+
+	return cnt;
+}
+
+int main() {
+	ListNode* head = NULL;
+
+	head = insert_last(head,20);
+	head = insert_last(head, 30);
+	head = insert_last(head, 40);
+	head = insert_first(head, 10);
+	print_list(head);
+	printf("\n > element count = %d\n", count_list(head));
+}
